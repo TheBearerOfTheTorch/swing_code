91f0e7d0b902a723e54a1859ecff66b3fd80d391
TheBearerOfTheTorch <iithindia@gmail.com>
2020-12-05 12:40:51 +0200

    commiting from main

---
+++ java_swing/src/tumieproject/NewClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tumieproject;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author faith
+ */
+public class NewClass {
+    
+//    void method()
+//    {
+//        foodMenu menu = new foodMenu();
+//        
+//        String 
+//    }
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+        JOptionPane.showMessageDialog(null, "first button is pressed");
+    }
+}
